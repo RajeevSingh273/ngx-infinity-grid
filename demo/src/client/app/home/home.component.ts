81f53f2666f689849af56113bd77dd9738c0111f
apoterenko <a.poterenko@gmail.com>
2017-04-29 20:57:35 +0300

    *update demo

--- demo/src/client/app/home/home.component.ts
+++ demo/src/client/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { NameListService } from '../shared/name-list/name-list.service';
+import {Component} from '@angular/core';
 
 import {
     InfinityPage,
@@ -16,9 +15,6 @@ class DataProvider {
     }
   }
 
-  /**
-   * @override
-   */
   public fetch(page: InfinityPage): Promise<InfinityPageData<string>> {
     return new Promise<InfinityPageData<string>>((resolve) => {
       setTimeout(() => {
@@ -27,68 +23,28 @@ class DataProvider {
           rawData: this.buffer.slice(page.startIndex, page.endIndex + 1),
           totalLength: this.buffer.length
         });
-      }, 3000);
+      }, 1000);
     });
   }
 }
 
-/**
- * This class represents the lazy loaded HomeComponent.
- */
 @Component({
   moduleId: module.id,
   selector: 'sd-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  newName: string = '';
-  errorMessage: string;
-  names: any[] = [];
   private dataProvider: DataProvider;
   private pageData: InfinityPageData<string>;
 
-  /**
-   * Creates an instance of the HomeComponent with the injected
-   * NameListService.
-   *
-   * @param {NameListService} nameListService - The injected NameListService.
-   */
-  constructor(public nameListService: NameListService) {
-    this.dataProvider = new DataProvider();
-  }
-
-  /**
-   * Get the names OnInit
-   */
-  ngOnInit() {
-    this.getNames();
-  }
-
-  /**
-   * Handle the nameListService observable
-   */
-  getNames() {
-    this.nameListService.get()
-      .subscribe(
-        names => this.names = names,
-        error => this.errorMessage = <any>error
-      );
-  }
-
-  /**
-   * Pushes a new name onto the names array
-   * @return {boolean} false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    // TODO: implement nameListService.post
-    this.names.push(this.newName);
-    this.newName = '';
-    return false;
+  constructor() {
+    this.dataProvider = new DataProvider();  // Inject your data provider here
   }
 
   private onFetchPage(page: InfinityPage) {
+
     // Flux action should be started here
     // Local pageData object should be updated during Flux-lifecycle
     // When long asynchronous request has been started, we should show loading rows in grid
